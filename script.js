c52f7e916507c647fb45a2e10c41effe819f5f2e
alessaduarte <alessadanubia@gmail.com>
2022-09-16 13:45:35 -0300

    Projeto 1 - Atualizado

--- script.js
+++ script.js
@@ -1,36 +1,6 @@
 let dados = []; 
 
 const elementoH1 = document.querySelector('h1');
-elementoH1.innerText = `${dados.length} monstros`;
-
-  fetch("https://jsonplaceholder.typicode.com/users", {
-    method: "GET",
-  })
-    .then((response) => {
-      if(response.status === 200) alert('Sucesso')
-      else alert('Algo deu errado')
-      return response.json();
-    })
-    .then((json) => {
-      dados = json;
-      console.log(json)
-
-for (let objeto of dados) {
-  const elemtArticle = document.createElement("article");
-  elemtArticle.classList.add("monstros");
-
-  elemtArticle.innerHTML = `
-  <img src="${objeto.image}" alt="monstro" />
-    <div>
-      <h2>${objeto.name}</h2>
-      <p>${objeto.email}</p>
-    </div>
-  `;
-
-  elementoH1.insertAdjacentElement("afterend", elemtArticle);
-}
-});
-
 const btn = document.querySelector('.btn-azul');
 
 btn.addEventListener("click", () => {
@@ -41,3 +11,30 @@ btn.addEventListener("click", () => {
     dados = [];
     elementoH1.innerText = `${dados.length} monstros`;
 });
+
+async function renderizar() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const dadosJSON = await response.json();
+
+  dados = dadosJSON.slice(0, 5);
+  elementoH1.innerText = `${dados.length} monstros`;
+
+  for (let objeto of dados) {
+    const elemtArticle = document.createElement("article");
+    elemtArticle.classList.add("monstros");
+
+    elemtArticle.innerHTML = `
+    <img src="https://robohash.org/${objeto.id}?set=set2" alt="monstro" />
+      <div>
+        <h2>${objeto.name}</h2>
+        <p>${objeto.email}</p>
+      </div>
+    `;
+
+    elementoH1.insertAdjacentElement("afterend", elemtArticle);
+    }
+  }
+
+renderizar();
+
+console.loh("primeiro");
